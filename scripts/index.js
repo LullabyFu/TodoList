891003d4ec0c5f16351c2e20bd4afe85f5991612
LullabyFu <1303072425@qq.com>
2019-03-29 23:56:45 +0800

    update4

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,67 @@
-function AddTodoList(){
-    alert("Add");
-    return true;
+'use strict';
+var todoList = [];
+var doneList = [];
+function submitTodo(){
+    var text = document.getElementById("addNew");
+    addNewTodoList(text.value);
+    text.value="";
+}
+
+function addNewTodoList(listContent){
+    var todo = document.createElement('li');
+    todo.className='todo';
+    todo.innerHTML="<input type='checkbox' class='box' onchange='addToAnotherList(this)'/>"+
+    "<p>"+listContent+"</p>"+
+    "<a onclick='deleteMission(this.parentElement)'></a>";
+    //console.log(todo);
+    var listTodo = document.getElementById('ListTodo');
+    //console.log(listTodo);
+    listTodo.appendChild(todo);
+    todoList.push(todo);
+}
+
+function addToAnotherList(box){
+    var li = box.parentElement;
+    if(li.getAttribute("class")=="todo"){
+        todoList.splice(todoList.indexOf(li),0);
+        var listDone = document.getElementById("ListDone");
+        li.setAttribute('class','done');
+        var temp = listDone.children;
+        listDone.insertBefore(li,temp[1]);
+        doneList.shift(li);
+    }
+    else{
+        doneList.splice(doneList.indexOf(li),0);
+        li.setAttribute('class','todo');
+        var listTodo = document.getElementById("ListTodo");
+        listTodo.appendChild(li);
+        todoList.push(li);
+    }
+}
+
+function deleteMission(mission){
+    if(mission.getAttribute('class')=='todo'){
+        todoList.splice(todoList.indexOf(mission),0);
+    }
+    else{
+        doneList.splice(doneList.indexOf(mission),0);
+    }
+    var list = mission.parentElement;
+    list.removeChild(mission);
+}
+
+window.load = load();
+
+function load(){
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://szuwechat.cn/tech", true);
+    xhr.send('username=szu&password=666');
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState==4){
+            console.log(xhr.responseText);
+        }
+        else{
+            console.log('loading');
+        }
+    }
 }
